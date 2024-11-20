6f0b5ccc0dddc0a198ef3c27082ad70d5b28eac6
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-11-20 15:12:20 +0530

    updates

--- ai-lms/tailwind.config.js
+++ ai-lms/tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
           foreground: 'hsl(var(--popover-foreground))'
         },
         primary: {
-          DEFAULT: 'hsl(var(--primary))',
+          DEFAULT: '#305CDE',//blue
           foreground: 'hsl(var(--primary-foreground))'
         },
         secondary: {
